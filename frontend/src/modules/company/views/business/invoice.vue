632ce1e561a252e2ee88a56da2cc3e56e2ecfbff
NkCyz <2212113@mail.nankai.edu.cn>
2026-04-09 23:48:02 +0800

    商务管理更名销售管理；首页数据看板前端占位、开票管理基础开发、交付管理基础开发

---
+++ frontend/src/modules/company/views/business/invoice.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineOptions({
+	name: 'company-business-invoice'
+});
+
+import BusinessLayout from '../business.vue';
+import InvoicePage from '../invoice.vue';
+</script>
+
+<template>
+	<BusinessLayout>
+		<InvoicePage />
+	</BusinessLayout>
+</template>
