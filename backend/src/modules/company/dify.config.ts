ea1feda68856c7a6b79fc50e5e0dddf8a8dfa421
AnthonyDeng121 <neymarjuniormzndhd@gmail.com>
2026-04-26 20:44:52 +0800

    chore: 恢复配置文件

---
+++ backend/src/modules/company/dify.config.ts
@@ -0,0 +1,65 @@
+/**
+ * Dify 工作流配置文件
+ */
+
+export interface DifyInputConfig {
+  name: string;
+  required: boolean;
+  type: 'string' | 'number' | 'boolean';
+}
+
+export interface DifyWorkflowConfig {
+  key: string;
+  name: string;
+  apiKey: string;
+  inputs: DifyInputConfig[];
+}
+
+export default [
+  {
+    key: 'materialQuote',
+    name: '物料智能询价',
+    apiKey: 'app-DwJlTWY7azTC8osHFmDZ0IkR', // TODO
+    inputs: [
+      { name: 'brand', required: true, type: 'string' },
+      { name: 'name', required: true, type: 'string' },
+      { name: 'dimension', required: true, type: 'string' },
+    ],
+  },
+  {
+    key: 'customerInfo',
+    name: '客户信息提供',
+    apiKey: 'app-3wX5QOPsMa1JiwUHBHsh49Hg', // TODO
+    inputs: [{ name: 'name', required: true, type: 'string' }],
+  },
+  {
+    key: 'receptionScript',
+    name: '客户接待话术',
+    apiKey: 'app-0Hu24lwKapNjZQZoZYljILnf', // TODO
+    inputs: [{ name: 'user_name', required: true, type: 'string' }],
+  },
+  {
+    key: 'supplierRecommend',
+    name: '供应商推荐',
+    apiKey: 'app-fO76RrFaAA2DAODImOVcmYVs', // TODO
+    inputs: [
+      { name: 'name', required: true, type: 'string' },
+      { name: 'dimension', required: true, type: 'string' },
+    ],
+  },
+  {
+    key: 'leadAnalysis',
+    name: '线索智能分析',
+    apiKey: 'app-wvN7QZhMFjMa808dEWEDszCN', // TODO: 替换为实际的API Key
+    inputs: [
+      { name: 'title', required: true, type: 'string' },
+      { name: 'detail', required: true, type: 'string' },
+    ],
+  },
+  {
+    key: 'customerPortrait',
+    name: '客户画像生成',
+    apiKey: 'TODO_REPLACE_WITH_ACTUAL_KEY', // TODO: 等待焕峰分配 Key
+    inputs: [{ name: 'name', required: true, type: 'string' }],
+  },
+] as DifyWorkflowConfig[];
